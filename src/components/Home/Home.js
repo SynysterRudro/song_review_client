6990d97ee0643fd4eb396e7b0de1ae9faaaba0b6
SynysterRudro <rudrosingh82@gmail.com>
2023-02-27 10:59:56 +0600

    banner completed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import banner from '../../assests/img/banner.jpg'
 
 const Home = () => {
+
+    const myStyle = {
+        backgroundImage:
+            ` linear-gradient(rgba(180,30 ,25, 50), rgba(0, 0, 0, 0.5)),url(${banner})`,
+        height: '80vh',
+
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+
+    };
+
     return (
-        <div>
-            I am from home
+        <div className='my-6'>
+            <div className='flex justify-center items-center' style={myStyle}>
+                <h1 className='text-white text-center text-5xl font-serif'> Welcome to Music Mania
+                    <span className='block text-xl'>Here you can give and watch song reviews</span>
+                </h1>
+            </div>
         </div>
     );
 };
